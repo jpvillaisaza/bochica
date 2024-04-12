afd9fc577be1ad50aff4a427425355cd8b596e51
Juan Pedro Villa Isaza <584947+jpvillaisaza@users.noreply.github.com>
2024-04-12 13:28:21 -0500

    Limit feed link discovery to head (#3)

--- src/Bochica.hs
+++ src/Bochica.hs
@@ -6,6 +6,9 @@ import Data.Maybe (fromMaybe, isJust, mapMaybe)
 import System.Environment (getArgs)
 import System.Exit (die)
 
+-- bytestring
+import Data.ByteString.Lazy (ByteString)
+
 -- http-client
 import Network.HTTP.Client (httpLbs, requestFromURI, responseBody)
 
@@ -17,6 +20,7 @@ import Network.URI (URI, parseURI, parseURIReference, uriScheme)
 
 -- tagsoup
 import Text.HTML.TagSoup (Tag, fromAttrib, isTagOpenName, parseTags)
+import Text.HTML.TagSoup.Match (getTagContent)
 
 -- utf8-string
 import qualified Data.ByteString.Lazy.UTF8 as LBSU8 (toString)
@@ -27,7 +31,7 @@ main = do
   args <- getArgs
   case args of
     [htmlUrl] -> do
-      eFeedLinks <- discoverFeedLinks htmlUrl
+      eFeedLinks <- fetchAndDiscoverFeedLinks htmlUrl
       case eFeedLinks of
         Left err ->
           die err
@@ -39,19 +43,25 @@ main = do
     _ ->
       die "usage: bochica <html-url>"
 
-discoverFeedLinks :: String -> IO (Either String [FeedLink])
-discoverFeedLinks htmlUrlStr =
+fetchAndDiscoverFeedLinks :: String -> IO (Either String [FeedLink])
+fetchAndDiscoverFeedLinks htmlUrlStr =
   case parseURI htmlUrlStr of
     Just htmlUrl | uriScheme htmlUrl `elem` ["http:", "https:"] -> do
       request <- requestFromURI htmlUrl
       manager <- newTlsManager
       response <- httpLbs request manager
-      let tags = parseTags (LBSU8.toString (responseBody response))
-      pure (Right (mapMaybe parseFeedLink tags))
+      pure (Right (discoverFeedLinksInHtml (responseBody response)))
     Just _ ->
       pure (Left "Invalid scheme")
     Nothing ->
       pure (Left "Invalid URL")
+
+discoverFeedLinksInHtml :: ByteString -> [FeedLink]
+discoverFeedLinksInHtml =
+  mapMaybe parseFeedLink
+    . getTagContent "head" (const True)
+    . parseTags
+    . LBSU8.toString
 
 data FeedType
   = Atom
